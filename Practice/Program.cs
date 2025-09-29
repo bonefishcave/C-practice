ae6993ff96d5aeebadaf05e324af7058f7f5524b
bonefishcave <brucepaynej231@kesbath.com>
2025-09-29 11:43:23 +0000

    Made ts

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -2,6 +2,44 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, world!");
+        chessBoardGame(args);
+    }
+
+    static void chessBoardGame(string[] args)
+    {
+        string[,] chessboard =
+        {
+            { "R", "K", "B", "Q", "Ki", "B", "K", "R" },
+            { ".", ".", ".", ".", ".", ".", ".", "." },
+            { ".", ".", ".", ".", ".", ".", ".", "."},
+            { ".", ".", ".", ".", ".", ".", ".", "."},
+            {".", ".", ".", ".", ".", ".", ".", "."},
+            {".", ".", ".", ".", ".", ".", ".", "."},
+            {".", ".", ".", ".", ".", ".", ".", "."},
+            { "R", "K", "B", "Ki", "Q", "B", "K", "R"}
+
+        };
+
+        //int length = chessboard.Length;
+        //Console.WriteLine(length);
+
+        try
+        {
+            for (int width = 0; width < chessboard.Length; width++)
+            {
+                for (int height = 0; height < chessboard.Length; height++)
+                {
+                    using StreamWriter writer = new StreamWriter("chessboard.txt");
+                    writer.WriteLine(chessboard[width, height]);
+                }
+            }
+
+        }
+        catch (Exception errorType)
+        {
+            Console.WriteLine("The error is: " + errorType.Message);
+        }
     }
 }
+
+    
--- Practice/obj/Debug/net8.0/Practice.AssemblyInfo.cs
+++ Practice/obj/Debug/net8.0/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81ccbb20d72a141cc4342ed2b27a615143ecf11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be804276161475411ec4b5f9004c2bb0d50517c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
